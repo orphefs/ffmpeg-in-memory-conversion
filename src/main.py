16ec60a5d220a9a8af9ded896af2bdc4e1911d92
Orfeas Kypris <kypriso@gmail.com>
2022-12-09 12:46:33 +0200

    compare both methods

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from numpy._typing import NDArray
 from pydub import AudioSegment
 import soundfile as sf
 
+
 def st_time(func):
     """
         st decorator to calculate the total time of a func
@@ -21,38 +22,32 @@ def st_time(func):
 
     return st_func
 
-# placeholder
-def write_to_mp3(array: NDArray, sampling_rate: int, path_to_mp3: str, normalized=True):
-    channels = 2 if (array.ndim == 2 and array.shape[1] == 2) else 1
-    if normalized:  # each item in the array should be a float in [-1, 1)
-        y = np.int16(array * 2 ** 15)
-    else:
-        y = np.int16(array)
-    song = AudioSegment(y.tobytes(), frame_rate=sampling_rate, sample_width=2, channels=channels)
-    song.export(path_to_mp3, format="mp3", bitrate="192k")
-
-
-def main():
-    # load wav audio
-    # audio = sf.read("/opt/data/wav/BE6JP2000005.wav")
-    audio = sf.read("/opt/data/wav/AEA040700549.wav")
-    audio = sf.read("/opt/data/wav/FR0NT1703090.wav")
-
-    # un-normalize array and convert to int16
-    array_in = np.int16(audio[0] * 2 ** 15)
-
-    # specify output filename
-    out_filename = "/tmp/test.mp3"
-
-    # init ffmpeg process
-    run_stream_conversion(array_in, out_filename)
 
 @st_time
-def run_stream_conversion(array_in: NDArray, out_filename: str):
+def run_pydub_conversion(array_in: NDArray, sampling_rate: int, path_to_mp3: str, bitrate: str,
+                         normalized=True):
+    channels = 2 if (array_in.ndim == 2 and array_in.shape[1] == 2) else 1
+    if normalized:  # each item in the array should be a float in [-1, 1)
+        y = np.int16(array_in * 2 ** 15)
+    else:
+        y = np.int16(array_in)
+    song = AudioSegment(y.tobytes(), frame_rate=sampling_rate, sample_width=2, channels=channels)
+    song.export(path_to_mp3, format="mp3", bitrate=bitrate)
+
+
+@st_time
+def run_stream_conversion(array_in: NDArray, sampling_rate: int, path_to_mp3: str, bitrate: str,
+                          normalized=True):
+    channels = 2 if (array_in.ndim == 2 and array_in.shape[1] == 2) else 1
+    if normalized:  # each item in the array should be a float in [-1, 1)
+        array_in = np.int16(array_in * 2 ** 15)
+    else:
+        array_in = np.int16(array_in)
+
     ffmpeg_process = (
         ffmpeg
-        .input('pipe:', format='s16le', ac=2, ar='44.1k')
-        .output(out_filename, acodec='mp3', format='s16le', audio_bitrate="192k")
+        .input('pipe:', format='s16le', ac=channels, ar=sampling_rate)
+        .output(path_to_mp3, acodec='mp3', format='s16le', audio_bitrate=bitrate)
         # .output(out_filename, acodec='pcm_s16le', ac=2, ar='44.1k')
         .overwrite_output()
         .run_async(pipe_stdin=True)
@@ -71,5 +66,22 @@ def run_stream_conversion(array_in: NDArray, out_filename: str):
     ffmpeg_process.wait()
 
 
+def main(path_to_wav: str):
+    # load wav audio
+    audio = sf.read(path_to_wav)
+
+    # specify output filename
+    out_filename = "/tmp/test.mp3"
+
+    # init ffmpeg process
+    run_stream_conversion(audio[0], 44100, out_filename, "320k")
+    run_pydub_conversion(audio[0], 44100, out_filename, "320k")
+
+
 if __name__ == '__main__':
-    main()
+    # path_to_wav = "/opt/data/wav/BE6JP2000005.wav"
+    # path_to_wav = "/opt/data/wav/AEA040700549.wav"
+    # path_to_wav = "/opt/data/wav/FR0NT1703090.wav"
+    path_to_wav = "/opt/data/wav/DEF078003170.wav"
+
+    main(path_to_wav)
